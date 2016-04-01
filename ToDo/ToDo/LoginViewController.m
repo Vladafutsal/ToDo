44f5ca4a7bd2bf8c2162c587c771c7d3d819b5e9
Cubes School 1 <v.vladimir980@gmail.com>
2016-04-01 15:55:36 +0200

    LginViewController almost completad

--- ToDo/ToDo/LoginViewController.m
+++ ToDo/ToDo/LoginViewController.m
@@ -8,5 +8,57 @@
 
 #import "LoginViewController.h"
 
+@interface LoginViewController()
+@property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@end
+
 @implementation LoginViewController
+
+#pragma mark - Pribvate API
+
+-(void)configureTextField:(UITextField *)textField {
+    if (textField.placeholder.length > 0) {
+        
+        //UIColor *color = [UIColor colorWithRed:117.0/255.0
+                    //                     green:<#(CGFloat)#>
+                    //                      blue:<#(CGFloat)#>
+                    //                     alpha:1.0;
+                                                  
+                                                  
+                                                  
+                            }
+        NSDictionary *attributes = @{
+                            NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Regular" size:14.0],
+                            NSForegroundColorAttributeName: [UIColor whiteColor]
+                            };
+        
+        
+        textField.attributedPlaceholder = [[NSAttributedString alloc] initWhithString:textField.placeholder attributes:attributes];
+    }
+    
+}
+
+
+#pragma mark - Actions
+
+- (IBAction)forgotPasswordButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)signInButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)signUpButtonTapped:(UIButton *)sender {
+    NSLog(@"Sign up...");
+}
+
+
+#pragma mark - View Lifecycle 
+
+ -(void)viewDidLoad {
+    [super viewDidLoad];
+                }
+
 @end
