9b45f5525d62348114be3a499cfb56da4d184927
Cubes School 1 <v.vladimir980@gmail.com>
2016-04-06 16:19:21 +0200

    Signup new project added.

--- ToDo/ToDo/SignUpViewController.m
+++ ToDo/ToDo/SignUpViewController.m
@@ -8,5 +8,26 @@
 
 #import "SignUpViewController.h"
 
+@interface SignUpViewController()
+@property (weak, nonatomic) IBOutlet UILabel *signUpLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *emailImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *birthdayImageView;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *birthdayTextField;
+@property (weak, nonatomic) IBOutlet UIButton *joinButton;
+@end
+
 @implementation SignUpViewController
+
+#pragma mark - Actions
+
+- (IBAction)backButtonTapped:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
 @end
